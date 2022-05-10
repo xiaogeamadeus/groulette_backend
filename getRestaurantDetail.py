aea9b1b1e388a7c66e3d55e1fa1de5fea7d9477a
XiaoGeAmadeus <xiaogeamadeus@gmail.com>
2022-05-10 16:53:42 +0900

    getRestaurantDetail.py renew

--- getRestaurantDetail.py
+++ getRestaurantDetail.py
@@ -5,20 +5,38 @@
 import json
 import requests
 
+def txt2list(filename):
+    """txt2list _summary_
+    txtファイル内のplace_idをlistとして読み込み
+    Args:
+        filename (String): ファイル名
+    Returns:
+        List: place_idのlist
+    """
+    with open(filename, 'r') as f:
+        data = f.read()
+        data2list = data.split('\n')
+        return data2list
+
 class getRestaurantDetail:
-
-    miseName = ["Chinese"]
+    miseName = ["Chinese1","Chinese2","Ramen","Fast","Pasta","Italian","Humberger","Gyudon","Syokudo","Teishoku","Family",
+                "Udon","Noodles","Franch","Asian","Korean","Pizza","Yakiniku","Pizza","Tempura","cafe","Health",
+                "Vegan","Yakitori", "Izakaya","Sushi"]
     API_KEY = "AIzaSyCh7xnktNn-nWh_KlNh_VP8rg8DITmqL9Q"
-
-    f = open(f"/users/xiaogeamadeus/mypy/Practice_of_Information_System/groulette_backend/restaurantData/kafugenDetail.txt",
-             mode='a+')
     payload = {}
     headers = {}
 
-    url = "https://maps.googleapis.com/maps/api/place/details/json?" \
-              "place_id=ChIJ-TIGS_EJAWARlNerteIGYsc&language=ja&" \
+    for i in miseName:
+      placeIds = txt2list(f"/users/xiaogeamadeus/mypy/Practice_of_Information_System/groulette_backend/restaurantPlaceid/{i}.txt")
+
+      for id in placeIds:
+          url = "https://maps.googleapis.com/maps/api/place/details/json?" \
+              f"place_id={id}&language=ja&" \
               f"key={API_KEY}"
 
-    response = requests.request("GET", url, headers=headers, data=payload)
-    num = f.write(response.text)
+          response = requests.request("GET", url, headers=headers, data=payload)
+          f = open(f"/users/xiaogeamadeus/mypy/Practice_of_Information_System/groulette_backend/restaurantPlaceDetail/{i}.json",
+             mode='a+')
+          num = f.write(response.text)
+
     f.close()
