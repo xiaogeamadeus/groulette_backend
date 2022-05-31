8fc88e8a56e2a057de9bfd12bc7ec3789f192e9d
XiaoGeAmadeus <xiaogeamadeus@gmail.com>
2022-05-31 16:32:47 +0900

    delete useless folder

--- restaurantDB/sql/query.sql
+++
@@ -1,4 +0,0 @@
--- DELETE FROM restaurants WHERE place_id = 'ChIJgR_qY1cIAWAR_G-414hVF-A'
-SELECT place_id, name, genre
-FROM restaurants
-WHERE genre in (0,1,2,3,4)
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-with open('groulette_backend/restaurantDB/restaurant.pkl', 'rb') as f:
-#     l = pickle.load(f)
